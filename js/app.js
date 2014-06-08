934368807667b3173eb2528aec748d7055a8d869
Sebastian Sulinski <info@ssdtutorials.com>
2014-06-08 10:45:35 +0100

    New jquery plugin created

--- js/app.js
+++ js/app.js
@@ -1,5 +1,5 @@
 $(function() {
 
-
+    $('#leftNavigation').ssdVerticalNavigation();
 
 });
--- js/jquery.ssd-vertical-navigation.js
+++ js/jquery.ssd-vertical-navigation.js
@@ -1,7 +1,142 @@
 ;(function($) {
 
-    $.fn.ssdVerticalNavigation = function() {
+    $.fn.ssdVerticalNavigation = function(options) {
 
+        "use strict";
+
+        var settings = $.extend({
+
+            classMaster : 'master',
+            classActive : 'active',
+            replaceWithAnchorText : true,
+            replaceElement : 'h1'
+
+        }, options);
+
+
+
+        function _isEmpty(thisValue) {
+
+            "use strict";
+
+            if ($.isArray(thisValue)) {
+
+                return (thisValue.length <= 0);
+
+            } else {
+
+                return (
+                    thisValue === '' ||
+                    thisValue === null ||
+                    typeof thisValue === 'undefined'
+                );
+
+            }
+
+        }
+
+
+        function _shouldReplaceHeading(thisHeading) {
+
+            "use strict";
+
+            return (
+                settings.replaceWithAnchorText &&
+                !_isEmpty(thisHeading)
+            );
+
+        }
+
+
+        function _replaceHeading(thisHeading) {
+
+            "use strict";
+
+            if (_shouldReplaceHeading(thisHeading)) {
+
+                $(settings.replaceElement).html(thisHeading.trim());
+
+            }
+
+        }
+
+
+        function _leftNavigationActiveMain(thisLi) {
+
+            "use strict";
+
+            thisLi
+                .toggleClass(settings.classActive)
+                .siblings()
+                .removeClass(settings.classActive);
+
+        }
+
+
+        function _leftNavigationActiveSub(thisLi) {
+
+            "use strict";
+
+            thisLi
+                .addClass(settings.classActive)
+                .siblings()
+                .removeClass(settings.classActive)
+                .closest('ul')
+                .closest('li')
+                .siblings()
+                .find('li')
+                .removeClass(settings.classActive);
+
+            _replaceHeading(thisLi.text());
+
+        }
+
+
+        function _leftNavigationClick(thisParentUl, thisLi) {
+
+            "use strict";
+
+            if (thisParentUl.hasClass(settings.classMaster)) {
+
+                _leftNavigationActiveMain(thisLi);
+
+            } else {
+
+                _leftNavigationActiveSub(thisLi);
+
+            }
+
+        }
+
+
+        return this.each(function() {
+
+            "use strict";
+
+            $(this)
+                .addClass(settings.classMaster)
+                .on('click',  'li a', function(event) {
+
+                try {
+
+                    event.preventDefault();
+                    event.stopPropagation();
+
+                    var thisA = $(this),
+                        thisLi = thisA.parent('li'),
+                        thisParentUl = thisLi.parent('ul');
+
+                    _leftNavigationClick(thisParentUl, thisLi);
+
+                } catch (errorMessage) {
+
+                    console.log(errorMessage);
+
+                }
+
+            });
+
+        });
 
 
     }
